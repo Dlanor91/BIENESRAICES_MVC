a922d57154da3f28cd41138c09b96ffd45edbcc5
developerscdeveloperscreditosdirectos@gmail.comreditosdirectos@gmail.com <r_lima@PCCDDS2>
2024-01-11 11:36:46 -0300

    formulario con errores

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -14,15 +14,48 @@ const formularioRegistro = (req, res) => {
 }
 
 const registrar = async (req, res) => {
+  console.log(req.body)
   //validacion
   await check('nombre')
     .notEmpty()
     .withMessage('El nombre no puede ir vacío')
     .run(req)
 
+  await check('email')
+    .notEmpty()
+    .withMessage('El email no puede ir vacío')
+    .run(req)
+
+  await check('email')
+    .isEmail()
+    .withMessage('El email no tiene un formato válido')
+    .run(req)
+
+  await check('password')
+    .isLength({ min: 6 })
+    .withMessage('El password debe ser de al menos 6 caracteres')
+    .run(req)
+
+  await check('repetir_password')
+    .equals('password')
+    .withMessage('Los passwords deben ser iguales')
+    .run(req)
+
   let resultado = validationResult(req)
 
-  res.json(resultado.array())
+  //Verificar que el resultado no este vacío, o sea hay errores
+  if (!resultado.isEmpty()) {
+    return res.render('auth/registro', {
+      pagina: 'Crear Cuenta',
+      errores: resultado.array(),
+      usuario:{
+        nombre: req.body.nombre,
+        email: req.body.email
+      }
+    })
+  }
+
+  res.json()
 
   const usuario = await Usuario.create(req.body)
 
