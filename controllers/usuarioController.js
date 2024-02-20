7176e316c841a6109460ce64327207d9153b482d
developerscdeveloperscreditosdirectos@gmail.comreditosdirectos@gmail.com <r_lima@PCCDDS2>
2024-02-20 09:39:28 -0300

    verificar password

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -30,6 +30,38 @@ const autenticar = async (req, res) => {
       errores: resultado.array(),
     })
   }
+
+  const { email, password } = req.body
+
+  //Comprobar si el usuario existe
+  const usuario = await Usuario.findOne({ where: { email } })
+  if (!usuario) {
+    return res.render('auth/login', {
+      pagina: 'Iniciar Sesión',
+      csrfToken: req.csrfToken(),
+      errores: [{ msg: 'El usuario ´no existe' }],
+    })
+  }
+
+  //Comprobar si el usuario esta confirmado
+  if (!usuario.confirmado) {
+    return res.render('auth/login', {
+      pagina: 'Iniciar Sesión',
+      csrfToken: req.csrfToken(),
+      errores: [{ msg: 'Tu cuenta no ha sido confirmada' }],
+    })
+  }
+
+  //Revisar el password
+  if (!usuario.verificarPassword(password)) {
+    return res.render('auth/login', {
+      pagina: 'Iniciar Sesión',
+      csrfToken: req.csrfToken(),
+      errores: [{ msg: 'El password es incorrecto' }],
+    })
+  }
+
+  //Autenticar al usuario
 }
 
 const formularioRegistro = (req, res) => {
