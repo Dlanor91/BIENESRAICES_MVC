7df4d8a4a27b76075dadea6b27f8ddf407d23ec9
Dlanor91 <limaronald926@gmail.com>
2024-03-04 15:37:08 -0300

    mostrando el mapa

--- public/js/mapa.js
+++ public/js/mapa.js
@@ -6,44 +6,60 @@
  * or disable the default devtool with "devtool: false".
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
  */
-/******/ (() => { // webpackBootstrap
-/******/ 	"use strict";
-/******/ 	var __webpack_modules__ = ({
-
-/***/ "./src/js/mapa.js":
-/*!************************!*\
+/******/ ;(() => {
+  // webpackBootstrap
+  /******/ 'use strict'
+  /******/ var __webpack_modules__ = {
+    /***/ './src/js/mapa.js':
+      /*!************************!*\
   !*** ./src/js/mapa.js ***!
   \************************/
-/***/ ((__unused_webpack___webpack_module__, __webpack_exports__, __webpack_require__) => {
+      /***/ (
+        __unused_webpack___webpack_module__,
+        __webpack_exports__,
+        __webpack_require__,
+      ) => {
+        eval(
+          "__webpack_require__.r(__webpack_exports__);\n(function() {\n    const lat = -34.915668;\n    const lng = -56.1620156;\n    const mapa = L.map('mapa').setView([lat, lng ], 14);\n    let marker;\n\n    //Utilizar Provider y Geocoder\n    const geocodeService = L.esri.Geocoding.geocodeService();\n\n    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {\n        attribution: '&copy; <a href=\"https://www.openstreetmap.org/copyright\">OpenStreetMap</a> contributors'\n    }).addTo(mapa);\n\n    //el pin\n    marker = new L.marker([lat, lng],{\n        draggable: true,\n        autoPan: true\n    })\n    .addTo(mapa)\n\n    //Detectar el movimiento del pin\n    marker.on('moveend',function(event){\n        marker = event.target\n\n        const posicion = marker.getLatLng();\n        \n        mapa.panTo(new L.LatLng(posicion.lat, posicion.lng))//centra el mapa\n\n        //Obtener la información de las calles al obtener el pin\n        geocodeService.reverse().latlng(posicion, 13).run(function(error, resultado){\n            marker.bindPopup(resultado.address.LongLabel)\n\n            //Llenar los campos\n            document.querySelector('.calle').textContent = resultado?.address?.Address ?? '';\n            document.querySelector('#calle').textCvalueontent = resultado?.address?.Address ?? '';\n            document.querySelector('#lat').textCvalueontent = resultado?.latlng?.lat ?? '';\n            document.querySelector('#lng').textCvalueontent = resultado?.latlng?.lng ?? '';\n        })\n    })\n\n})()\n\n//# sourceURL=webpack://bienesraices_mvc/./src/js/mapa.js?",
+        )
 
-eval("__webpack_require__.r(__webpack_exports__);\nalert('Algo mas aun')\n\n\n//# sourceURL=webpack://bienesraices_mvc/./src/js/mapa.js?");
+        /***/
+      },
 
-/***/ })
-
-/******/ 	});
-/************************************************************************/
-/******/ 	// The require scope
-/******/ 	var __webpack_require__ = {};
-/******/ 	
-/************************************************************************/
-/******/ 	/* webpack/runtime/make namespace object */
-/******/ 	(() => {
-/******/ 		// define __esModule on exports
-/******/ 		__webpack_require__.r = (exports) => {
-/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
-/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
-/******/ 			}
-/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
-/******/ 		};
-/******/ 	})();
-/******/ 	
-/************************************************************************/
-/******/ 	
-/******/ 	// startup
-/******/ 	// Load entry module and return exports
-/******/ 	// This entry module can't be inlined because the eval devtool is used.
-/******/ 	var __webpack_exports__ = {};
-/******/ 	__webpack_modules__["./src/js/mapa.js"](0, __webpack_exports__, __webpack_require__);
-/******/ 	
-/******/ })()
-;
+    /******/
+  }
+  /************************************************************************/
+  /******/ // The require scope
+  /******/ var __webpack_require__ = {}
+  /******/
+  /************************************************************************/
+  /******/ /* webpack/runtime/make namespace object */
+  /******/ ;(() => {
+    /******/ // define __esModule on exports
+    /******/ __webpack_require__.r = (exports) => {
+      /******/ if (typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+        /******/ Object.defineProperty(exports, Symbol.toStringTag, {
+          value: 'Module',
+        })
+        /******/
+      }
+      /******/ Object.defineProperty(exports, '__esModule', { value: true })
+      /******/
+    }
+    /******/
+  })()
+  /******/
+  /************************************************************************/
+  /******/
+  /******/ // startup
+  /******/ // Load entry module and return exports
+  /******/ // This entry module can't be inlined because the eval devtool is used.
+  /******/ var __webpack_exports__ = {}
+  /******/ __webpack_modules__['./src/js/mapa.js'](
+    0,
+    __webpack_exports__,
+    __webpack_require__,
+  )
+  /******/
+  /******/
+})()
--- src/js/mapa.js
+++ src/js/mapa.js
@@ -1,1 +1,47 @@
-alert('Algo mas aun')
+;(function () {
+  const lat = -34.915668
+  const lng = -56.1620156
+  const mapa = L.map('mapa').setView([lat, lng], 14)
+  let marker
+
+  //Utilizar Provider y Geocoder
+  const geocodeService = L.esri.Geocoding.geocodeService()
+
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  }).addTo(mapa)
+
+  //el pin
+  marker = new L.marker([lat, lng], {
+    draggable: true,
+    autoPan: true,
+  }).addTo(mapa)
+
+  //Detectar el movimiento del pin
+  marker.on('moveend', function (event) {
+    marker = event.target
+
+    const posicion = marker.getLatLng()
+
+    mapa.panTo(new L.LatLng(posicion.lat, posicion.lng)) //centra el mapa
+
+    //Obtener la información de las calles al obtener el pin
+    geocodeService
+      .reverse()
+      .latlng(posicion, 13)
+      .run(function (error, resultado) {
+        marker.bindPopup(resultado.address.LongLabel)
+
+        //Llenar los campos
+        document.querySelector('.calle').textContent =
+          resultado?.address?.Address ?? ''
+        document.querySelector('#calle').textCvalueontent =
+          resultado?.address?.Address ?? ''
+        document.querySelector('#lat').textCvalueontent =
+          resultado?.latlng?.lat ?? ''
+        document.querySelector('#lng').textCvalueontent =
+          resultado?.latlng?.lng ?? ''
+      })
+  })
+})()
