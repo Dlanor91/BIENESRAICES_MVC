8a78b57238f780fe847a12e71a78058d3d237be2
Dlanor91 <limaronald926@gmail.com>
2024-03-06 16:10:35 -0300

    seeder de propiedad

---
+++ models/index.js
@@ -0,0 +1,17 @@
+import Categoria from './Categoria.js'
+import Propiedad from './Propiedad.js'
+import Precio from './Precio.js'
+import Usuario from './Usuario.js'
+
+/*Precio.hasOne(Propiedad) *///La propiedad tiene un precio
+Propiedad.belongsTo(Precio) //la propiedad tiene un precio
+/*Propiedad.belongsTo(Precio, {foreignKey:'precioId'}) *///si quiero ponr un foreingKey presonalizado
+
+Propiedad.belongsTo(Categoria) //la propiedad tiene una categoria
+Propiedad.belongsTo(Usuario) //la propiedad tiene un precio
+export {
+    Propiedad,
+    Categoria,
+    Precio,
+    Usuario
+}
